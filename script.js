311005847815060e2a0b23b6c511bcac3ee4329f
Ashma <ashma6104@gmail.com>
2018-12-19 20:59:56 +0545

    changes added to js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var name1 = prompt("Enter one character name:");
-document.write(name1);
+var noun1 = prompt("Enter noun:");
+var noun2 = prompt("Enter another noun:");
+var color = prompt('Enter color name:');
+var pronoun = prompt('Enter pronoun name:');
+
+var str1 = noun1+" are red, ";
+var str2 = noun2+ " are "+ color;
+var str3 = pronoun+" belong to the zoo";
+document.write(str1);
+document.write(str2);
+document.write(str3);
+
+
